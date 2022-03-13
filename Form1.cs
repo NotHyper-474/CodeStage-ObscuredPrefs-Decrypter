7a37433dc14ffa3c1238e423d835faa0d25fb27c
NotHyper <survivaltemer@gmail.com>
2022-03-13 17:48:25 -0300

    Code port

---
+++ Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CodeStage_Decrypter
+{
+	public partial class DecrypterForm : Form
+	{
+		private string strPart = "De";
+		private string strPartOld = "En";
+
+		private static readonly Icon encryptIcon;
+		private static readonly Icon decryptIcon;
+
+		private bool valueMode;
+
+		public DecrypterForm()
+		{
+			InitializeComponent();
+		}
+
+		static DecrypterForm()
+		{
+			decryptIcon = Properties.Resources.decIcon;
+			encryptIcon = Properties.Resources.encIcon;
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			cryptoKeyBox.Text = Properties.Settings.Default.SavedKey;
+		}
+
+		private void DecrypterForm_Enter(object sender, EventArgs e)
+		{
+			Opacity = 1.00;
+		}
+
+		private void DecrypterForm_Leave(object sender, EventArgs e)
+		{
+			Opacity = 0.60;
+		}
+
+		private void encryptModeCheck_CheckedChanged(object sender, EventArgs e)
+		{
+			strPartOld = strPart;
+			if (strPart == "De")
+			{
+				strPart = "En";
+				Icon = encryptIcon;
+			}
+			else
+			{
+				strPart = "De";
+				Icon = decryptIcon;
+			}
+
+			textBox.PlaceholderText = textBox.PlaceholderText.Replace(strPartOld.ToLower(), strPart.ToLower());
+			decencButton.Text = decencButton.Text.Replace(strPartOld, strPart);
+		}
+
+		private void decencButton_Click(object sender, EventArgs e)
+		{
+			string text = textBox.Text;
+			char[] key = cryptoKeyBox.Text.ToCharArray();
+
+			if (encryptModeCheck.Checked)
+			{
+				if (!valueMode)
+					resultBox.Text = EncrypterDecrypter.Encrypt(text, key);
+				else resultBox.Text = EncrypterDecrypter.EncryptValue(text, new string(key));
+			}
+			else
+			{
+				if (valueMode)
+				{
+					byte[] result = EncrypterDecrypter.DecryptData(new string(key), text);
+					if ((result != null) && Encoding.UTF8.GetString(result) == "SAVES_TAMPERED")
+						MessageBox.Show("Could not decrypt value. The value was probably tampered with.");
+					var type = EncrypterDecrypter.GetRawDataType(text);
+					//MessageBox.Show(Enum.GetName(typeof(EncrypterDecrypter.DataType), type));
+					resultBox.Text = result.Length > 0 ? Encoding.UTF8.GetString(result) : string.Empty;
+					return;
+				}
+				if (!Base64Utils.IsBase64(text))
+				{
+					MessageBox.Show("Invalid Base64 text. Decrypting without decoding.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+					resultBox.Text = new string(EncrypterDecrypter.EncryptDecrypt(text.ToCharArray(), key));
+					return;
+				}
+				resultBox.Text = EncrypterDecrypter.Decrypt(text, key);
+			}
+		}
+
+		private void cryptoKeyBox_TextChanged(object sender, EventArgs e)
+		{
+			Properties.Settings.Default.SavedKey = cryptoKeyBox.Text;
+			Properties.Settings.Default.Save();
+		}
+
+		private void valueModeCheck_CheckedChanged(object sender, EventArgs e)
+		{
+			valueMode = valueModeCheck.Checked;
+		}
+	}
+}
---
+++ Utils/Base64Utils.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeStage_Decrypter
+{
+	internal static class Base64Utils
+	{
+		public static bool IsBase64(char[] b64)
+        {
+			try
+            {
+				Convert.FromBase64CharArray(b64, 0, b64.Length);
+				return true;
+            }
+			catch (Exception)
+            {
+				return false;
+            }
+        }
+
+		public static bool IsBase64(string b64)
+        {
+			return IsBase64(b64.ToCharArray());
+        }
+
+		public static string CharArrayToBase64(char[] array)
+		{
+			return Convert.ToBase64String(CharArrayToBytes(array));
+		}
+
+		public static string StringToBase64(string str)
+		{
+			return CharArrayToBase64(str.ToCharArray());
+		}
+
+		public static char[] FromBase64ToCharArray(char[] b64)
+        {
+			return BytesToCharArray(Convert.FromBase64CharArray(b64, 0, b64.Length));
+        }
+
+		public static char[] FromBase64ToCharArray(string b64)
+		{
+			return BytesToCharArray(Convert.FromBase64String(b64));
+		}
+
+		public static string FromBase64ToString(string b64)
+        {
+			return new string(FromBase64ToCharArray(b64));
+        }
+
+		public static byte[] CharArrayToBytes(char[] array)
+		{
+			return Encoding.UTF8.GetBytes(array);
+		}
+
+		public static char[] BytesToCharArray(byte[] bytes)
+		{
+			return Encoding.UTF8.GetChars(bytes);
+		}
+	}
+}
