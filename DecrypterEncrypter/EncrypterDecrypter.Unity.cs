d138e54e26c73f8e7483a7c960ff35dbb0b59a2e
Hyper_ <survivaltemer@gmail.com>
2023-07-18 19:52:59 -0300

    Remove submodule and include content into repo

---
+++ DecrypterEncrypter/EncrypterDecrypter.Unity.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeStage_Decrypter
+{
+	public static partial class EncrypterDecrypter
+	{
+		public struct Vector2Int
+		{
+			public int x;
+			public int y;
+
+			public override string ToString()
+			{
+				return '(' + x + ", " + y + ')';
+			}
+		}
+
+		public struct Vector3Int
+		{
+			public int x;
+			public int y;
+			public int z;
+
+			public override string ToString()
+			{
+				return '(' + x + ", " + y + ", " + z + ')';
+			}
+		}
+
+		public struct Vector2
+		{
+			public Vector2(float x, float y)
+			{
+				this.x = x;
+				this.y = y;
+			}
+
+			public float x;
+			public float y;
+
+			public override string ToString()
+			{
+				return '(' + x + ", " + y + ')';
+			}
+		}
+
+		public struct Vector3
+		{
+			public Vector3(float x, float y, float z)
+			{
+				this.x = x;
+				this.y = y;
+				this.z = z;
+			}
+
+			public float x;
+			public float y;
+			public float z;
+
+			public override string ToString()
+			{
+				return '(' + x + ", " + y + ", " + z + ')';
+			}
+		}
+
+		public struct Vector4
+		{
+			public Vector4(float x, float y, float z, float w)
+			{
+				this.x = x;
+				this.y = y;
+				this.z = z;
+				this.w = w;
+			}
+
+			public float w;
+			public float x;
+			public float y;
+			public float z;
+
+			public override string ToString()
+			{
+				return '(' + x + ", " + y + ", " + z + ", " + w + ')';
+			}
+		}
+
+		public struct Quaternion
+		{
+			public Quaternion(float x, float y, float z, float w)
+			{
+				this.x = x;
+				this.y = y;
+				this.z = z;
+				this.w = w;
+			}
+
+			public float w;
+			public float x;
+			public float y;
+			public float z;
+
+			public override string ToString()
+			{
+				return '(' + x + ", " + y + ", " + z + ", " + w + ')';
+			}
+		}
+
+		public struct Rect
+		{
+			public Rect(float x, float y, float width, float height)
+			{
+				this.x = x;
+				this.y = y;
+				this.width = width;
+				this.height = height;
+			}
+
+			public float x;
+			public float y;
+			public float width;
+			public float height;
+
+			public override string ToString()
+			{
+				return '(' + x + ", " + y + ", " + width + ", " + height + ')';
+			}
+		}
+
+		public struct Color
+		{
+			public float r;
+			public float g;
+			public float b;
+			public float a;
+
+			public override string ToString()
+			{
+				return '(' + r + ", " + g + ", " + b + ", " + a + ')';
+			}
+		}
+
+		public struct Color32
+		{
+			public Color32(byte r, byte g, byte b, byte a)
+			{
+				this.r = r;
+				this.g = g;
+				this.b = b;
+				this.a = a;
+			}
+
+			public byte r;
+			public byte g;
+			public byte b;
+			public byte a;
+
+			public override string ToString()
+			{
+				return "(R:" + r + ", G:" + g + ", B:" + b + ", A:" + a + ')';
+			}
+		}
+	}
+}
---
+++ DecrypterEncrypter/EncrypterDecrypter.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using CodeStage_Decrypter.Utils;
+
+namespace CodeStage_Decrypter
+{
+    public static partial class EncrypterDecrypter
+    {
+        public const string PrefsKey = "9978e9f39c218d674463dab9dc728bd6";
+
+        public static DeviceLockLevel lockToDevice = DeviceLockLevel.None;
+
+        internal static string deviceId;
+        public static string DeviceId
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(deviceId))
+                {
+                    deviceId = GetDeviceId();
+                }
+                return deviceId;
+            }
+            set => deviceId = value;
+        }
+
+        internal static uint deviceIdHash;
+        internal static uint DeviceIdHash
+        {
+            get
+            {
+                if (deviceIdHash == 0)
+                    deviceIdHash = CalculateChecksum(DeviceId);
+                return deviceIdHash;
+            }
+        }
+
+        public enum DeviceLockLevel : byte
+        {
+            None,
+            Soft,
+            Strict
+        }
+        
+        public static byte Version = 3;
+
+        internal static DataType GetRawDataType(string value)
+        {
+            var result = DataType.Unknown;
+            byte[] inputBytes;
+
+            try
+            {
+                inputBytes = Convert.FromBase64String(value);
+            }
+            catch
+            {
+                return result;
+            }
+
+            if (inputBytes.Length < 7)
+                return result;
+
+            var inputLength = inputBytes.Length;
+
+            result = (DataType)inputBytes[inputLength - 7];
+
+            var version = inputBytes[inputLength - 6];
+            if (version > 10)
+                result = DataType.Unknown;
+
+            return result;
+        }
+
+        public static string Encrypt(string value, char[] key)
+        {
+            return Encrypt(value.ToCharArray(), key);
+        }
+
+        internal static string Encrypt(char[] value, char[] key)
+        {
+            return Base64Utils.CharArrayToBase64(EncryptDecrypt(value, key));
+        }
+
+        internal static char[] Decrypt(char[] value, char[] key)
+        {
+            var decoded = Base64Utils.FromBase64ToCharArray(value);
+            return EncryptDecrypt(decoded, key);
+        }
+
+        public static string Decrypt(string value, char[] key)
+        {
+            return new(Decrypt(value.ToCharArray(), key));
+        }
+
+        private static char[] EncryptDecrypt(IReadOnlyList<char> value, IReadOnlyList<char> key)
+        {
+            var valueLength = value.Count;
+            var keyLength = key.Count;
+
+            var result = new char[valueLength];
+            for (int i = 0; i < valueLength; i++)
+            {
+                result[i] = (char)(value[i] ^ key[i % keyLength]);
+            }
+            return result;
+        }
+
+        private static byte[] EncryptDecryptBytes(IReadOnlyList<byte> value, IReadOnlyList<char> key)
+        {
+            var valueLength = value.Count;
+            var keyLength = key.Count;
+
+            var result = new byte[valueLength];
+            for (int i = 0; i < valueLength; i++)
+            {
+                result[i] = (byte)(value[i] ^ key[i % keyLength]);
+            }
+
+            return result;
+        }
+
+        private static byte[] EncryptDecryptBytesObsolete(IReadOnlyList<byte> value, string key)
+        {
+            var valueLength = value.Count;
+            var keyLength = key.Length;
+
+            var result = new byte[valueLength];
+            for (int i = 0; i < valueLength; i++)
+            {
+                result[i] = (byte)(value[i] ^ key[i % keyLength]);
+            }
+
+            return result;
+        }
+
+        private static string GetDeviceId()
+        {
+            var id = "";
+            // TODO: Simulate DeviceId for other platforms?
+            #if UNITY_IPHONE
+            id = UnityEngine.iOS.Device.vendorIdentifier;
+            #endif
+
+            #if !ACTK_PREVENT_READ_PHONE_STATE
+            if (string.IsNullOrEmpty(id)) id = SystemInfo.deviceUniqueIdentifier;
+            #endif
+            return id;
+        }
+    }
+}
